83fdb31dff07cb20aa25b5c4282e4bcc49d2ca14
AnesKostreba <aneskostreba@yahoo.com>
2022-11-03 16:11:17 +0100

    add functions

--- newfunctions1.js
+++ newfunctions1.js
@@ -110,8 +110,8 @@ console.log(numbers(1000));
 
 
     function reverse(str) {
-        let newString = "";
-        for (let i = str.length - 1; i >= 0; i--) {
+        var newString = "";
+        for (var i = str.length - 1; i >= 0; i--) {
             newString += str[i];
         }
         return newString;
