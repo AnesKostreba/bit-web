88ce40884d0c12917c2e6320714f0a4cf4708479
AnesKostreba <aneskostreba@yahoo.com>
2022-12-10 12:03:38 +0100

    add

--- Object/index.js
+++ Object/index.js
@@ -49,16 +49,44 @@ function max(array) {
     var maxPrice = 0
     for (var i = 0; i < array.length; i++) {
         var element = array[i];
-        if(element.price > maxPrice){
+        if (element.price > maxPrice) {
             maxPrice = element.price;
         }
     }
-    return element.product +" "+ maxPrice;
+    return element.product + " " + maxPrice;
 }
 console.log(max([
     { product: "Milk", quantity: 1, price: 333 },
-    { product: "Egs", quantity: 2, price: 211 },
+    { product: "bananas", quantity: 2, price: 211 },
     { product: "Egs", quantity: 2, price: 452 },
     { product: "Apple", quantity: 2, price: 2222 }
 ]));
 
+
+// 3. Given a word, create an object that stores the indexes of each letter in an array. Make
+// sure the letters are the keys. Make sure the letters are symbols. Make sure the indexes
+// are stored in an array and those arrays are values.
+
+function mapLetters(str='') {
+    if (!str || str.length == 0) {
+        return 0;
+    }
+
+    var result = {};
+
+    for (var i = 0; i < str.length; i++) {
+        var newArray = [];
+        newArray[0] = i;
+        for (var j = 0; j < str.length; j++) {
+            if (str[i] == str[j] && i != j) {
+                newArray.unshift(j);
+            }
+        }
+
+        result[str[i]] = newArray
+    }
+
+    return result;
+}
+
+console.log(mapLetters("grapes"));
