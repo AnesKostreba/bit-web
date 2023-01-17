9a6f9f141d3946025b0bb08dbc8786de036ab549
AnesKostreba <aneskostreba@yahoo.com>
2023-01-17 17:48:28 +0100

    add

--- DOM/index.js
+++ DOM/index.js
@@ -1,13 +1,36 @@
-function styling() {
-    var stl = document.getElementById("text");
-    stl.style.backgroundColor = "red";
-}
+// function styling() {
+//     var stl = document.getElementById("text");
+//     stl.style.backgroundColor = "red";
+// }
 
-var b =document.getElementById("text").children.length;
-console.log(b);
-console.log(styling());
+// var b =document.getElementById("text").children.length;
+// console.log(b);
+// console.log(styling());
 
-    var all = document.getElementsById("text");
-    all.childNodes[1].style.backgroundColor = "green";
+//     var all = document.getElementsById("text");
+//     all.childNodes[1].style.backgroundColor = "green";
     
-console.log(all);
+// console.log(all);
+
+var element1 = document.createElement("li");
+var textElement = document.createTextNode("Add element");
+element1.appendChild(textElement);
+document.getElementById("list").appendChild(element1);
+
+var prebaci = document.getElementById("text").firstElementChild;
+document.getElementById("list").appendChild(prebaci);
+
+
+
+var addNew = document.createElement("li");
+var textNodeNew = document.createTextNode("Novi Element u drugom UL");
+addNew.appendChild(textNodeNew);
+document.getElementById("text").appendChild(addNew);
+
+var noviGore = document.getElementById("text").lastElementChild;
+document.getElementById("list").appendChild(noviGore);
+
+var rem = document.getElementById("list");
+if(rem.hasChildNodes()){
+    rem.removeChild(rem.firstChild);
+}
--- Event Handlers/Chat/index.js
+++ Event Handlers/Chat/index.js
@@ -7,19 +7,38 @@ var forDeleting = document.querySelector('p');
 function sendMessage() {
     if (input.value) {
         var msg = document.createElement("p");
+        msg.setAttribute("id","paragraph");
         msg.textContent = input.value;
         mainArea.appendChild(msg);
         input.value = "";
     }
 };
 
-function reset() {
-    
-    forDeleting.remove();
+// doFoo.onclick = () => {
+//     const myNode = document.getElementById("chatt");
+//     myNode.innerHTML = '';
+//   }
+
+
+// res.onclick = () => {
+//     const myNode = document.getElementById("chatt");
+//     while (myNode.lastElementChild) {
+//         myNode.removeChild(myNode.lastElementChild);
+//       }
+// }
+
+
+
+function ress() {
+    const myNode = document.getElementById("chatt");
+    while (myNode.lastElementChild) {
+            myNode.removeChild(myNode.lastElementChild);
+      }
+}
 
     // var parentEl = forDeleting.element;
     // parentEl.remove(forDeleting);
-}
+
 // document.querySelector('.text').addEventListener('keypress', function (e) {
 //     if (e.key === 'Enter') {
 //         var msg = document.createElement("p");
