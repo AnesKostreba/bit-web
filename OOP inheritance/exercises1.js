068f78f22927fae47b8ff8b94a2cc3c8c1cec792
AnesKostreba <aneskostreba@yahoo.com>
2023-01-28 11:15:05 +0100

    add

--- OOP inheritance/exercises1.js
+++ OOP inheritance/exercises1.js
@@ -58,11 +58,11 @@ var car4 = new Car('Skoda','Octavia',113);
 // console.log(car1.checkMotion());
 
 
-console.log(car2.status());
-car2.accelrate(15);
-console.log(car2.status());
-car2.break(135);
-console.log(car2.status());
+// console.log(car2.status());
+// car2.accelrate(15);
+// console.log(car2.status());
+// car2.break(135);
+// console.log(car2.status());
 
 
 // console.log(car2.accelrate(15));
@@ -71,3 +71,72 @@ console.log(car2.status());
 
 // console.log(car3.status());
 // console.log(car4.status());
+
+
+
+
+
+
+
+
+
+
+// a)Create a TV class with properties like brand, channel and volume. Specify brand in a constructor parameter. Channel should be 1 by default. Volume should be 50 by default.
+// b) Add methods to increase and decrease volume. Volume can't never be below 0 or above 100.
+// c) Add a method to set the channel. Let's say the TV has only 50 channels so if you try to set channel 60 the TV will stay at the current channel.
+// d) Add a method to reset TV so it goes back to channel 1 and volume 50. (Hint: consider using it from the constructor).
+// e) It's useful to write a status, that returns info about the TV status like: "Panasonic at channel 8, volume 75".
+// f) Create a TV object and test it!
+
+
+
+class Tv{
+    constructor(brand, channel=1, volume = 50){
+        this.brand = brand;
+        this.channel = channel;
+        this.volume = volume;
+    }
+
+    status(){
+        return this.brand+" "+this.channel+" "+this.volume;
+    }
+
+    increaseVolume(volume){
+        if(this.volume > 1 && this.volume <= 100){
+            this.volume += volume;
+        }
+        return this.volume;
+    }
+
+
+    decreaseVolume(volume){
+        if(this.volume > 1 && this.volume <= 100){
+            this.volume -= volume;
+        }
+        if(this.volume < 0){
+            this.volume = 0;
+        }
+        return this.volume;
+    }
+
+
+    setChannel(newChannel){
+        if(newChannel > 1 && newChannel < 50){
+            this.channel = newChannel;
+        }
+        if(this.channel > 50){
+            this.channel = this.channel;
+        }
+    }
+
+    resetTv(){
+        this.channel=1;
+        this.volume=50;
+        return this;
+    }
+}
+
+var tv = new Tv('Tesla',5,33);
+tv.setChannel(56);
+console.log(tv.status());
+
